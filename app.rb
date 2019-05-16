b9cc509d3fcbacc52a4cd3a5866a35d3fe27067e
Leslie Osorio <lesosorio17@gmail.com>
2019-05-16 21:13:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,12 +13,5 @@ class App < Sinatra::Base
     "Hello #{@user_name}!"
   end
 
-  get '/medicines/:id' do
-    @medicine = all_the_medicines.select do |medicine|
-      medicine.id == params[:id]
-    end.first
-
-    erb :'/medicines/show.html'
-  end 
-
+  
 end
